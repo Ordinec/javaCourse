66af78ad995e050021986d44ad030ae4546a68ee
Serhii Moroz <sergiitst3@gmail.com>
2023-06-02 18:19:16 +0300

    Homework 2

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args){
+        int varA = 2;
+        int varB = 1;
+        System.out.println("Sum: " + (varA + varB));
+        System.out.println("Subtraction: " + (varA - varB));
+        System.out.println("Division: " + (varA / varB));
+        System.out.println("Multiplication: " + (varA * varB));
 
+        String value = "qwerrewq";
+        StringBuilder sb =new StringBuilder(value);
+        String valueReversed = sb.reverse().toString();
+        System.out.println(value.equals(valueReversed));
     }
 }
